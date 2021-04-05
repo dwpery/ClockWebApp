721100027e8e2c6480e4c8cb24cb72ca27cefeba
Pieter Edwards <PieterEd12@Gmail.com>
2021-04-05 13:34:40 +0100

    Update main.js

--- main.js
+++ main.js
@@ -17,6 +17,9 @@ var timerState = true; // true = Playing, false = Paused
 var ctx; // Used for drawring analog clock
 var date; // Used to get info for the analog clock
 var size = 95; // Represents size of analog clock
+var amountOfAlarms = 0; // Holds current amount of alarms
+var alarmNamesContainer = new Array; // Holds alarm names
+var alarmDescrContainer = new Array; // Holds alarm descriptions
 // Controls clock logic
 setInterval(function() {
      var today = new Date(); 
