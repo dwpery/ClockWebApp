73fc209480dc45a260fa120d4d53a292abe53842
SpicyPTV <PieterEd12@gmail.com>
2022-04-17 03:00:05 +0100

    Work on PWA infastructure, added servicde worker and finished web manifest

---
+++ code/pwabuilder-adv-sw.js
@@ -0,0 +1,4 @@
+
+  import { precacheAndRoute } from 'workbox-precaching/precacheAndRoute';
+
+  precacheAndRoute([{"revision":"8fb8fee4fcc3cc86ff6c724154c49c42","url":"jquery-3.6.0.min.js"},{"revision":"8a3a023acbff5f351ab79ec3d57d435b","url":"main.js"},{"revision":"66868045c2369a42b4af5721bf3cad9b","url":"master.css"}]);
