eba61b0c07d743fcf3799175b31031bafd550336
SpicyPTV <pietered12@gmail.com>
2023-07-12 23:09:21 +0100

    Milliseconds added to timer

--- public/code/main.js
+++ public/code/main.js
@@ -131,6 +131,7 @@ if (localStorage.getItem('doubleDigits') == "true") {
   $("#span4").html("00");
   $("#span5").html("00");
   $("#span6").html("00");
+  $("#span7").html("00");
   $("#hours").attr("placeholder", "00");
   $("#minutes").attr("placeholder", "00");
   $("#seconds").attr("placeholder", "00");
@@ -589,6 +590,7 @@ function doubleDigitsChanger() {
     $("#span4").html("00");
     $("#span5").html("00");
     $("#span6").html("00");
+    $("#span7").html("00");
     $("#hours").attr("placeholder", "00");
     $("#minutes").attr("placeholder", "00");
     $("#seconds").attr("placeholder", "00");
@@ -598,6 +600,7 @@ function doubleDigitsChanger() {
     $("#span4").html("0");
     $("#span5").html("0");
     $("#span6").html("0");
+    $("#span7").html("0");
     $("#hours").attr("placeholder", "0");
     $("#minutes").attr("placeholder", "0");
     $("#seconds").attr("placeholder", "0");
@@ -889,54 +892,58 @@ function themeChanger() {
 // Starts stopwatch
 function startStopwatch() {
   // Values
+  stopwatchMs = 0;
   stopwatchS = 0;
   stopwatchM = 0;
   stopwatchH = 0;
-
-  // Prints values
-  $("#span4").html(stopwatchH);
-  $("#span5").html(stopwatchM);
-  $("#span6").html(stopwatchS);
 
   // Prints values
   if (doubleDigits == "true") {
     $("#span4").html((stopwatchH < 10 ? "0" : "" ) + stopwatchH);
     $("#span5").html((stopwatchM < 10 ? "0" : "" ) + stopwatchM);
     $("#span6").html((stopwatchS < 10 ? "0" : "" ) + stopwatchS);
+    $("#span7").html((stopwatchMs < 10 ? "0" : "" ) + stopwatchMs);
   } else {
     $("#span1").html(stopwatchH);
     $("#span2").html(stopwatchM);
     $("#span3").html(stopwatchS);
+    $("#span7").html(stopwatchMs);
   }
 
   // Changes Icon
   $("#stopwatchPlayContainer").html('<svg class="buttonSVG" width="100" height="100" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg" id="pauseStopwatch" onclick="pauseStopwatch()"><circle class="circleOnSVG" cx="50" cy="50" r="50" fill="#C4C4C4"/><path class="barOnSVG" d="M33 30.5C33 27.4624 35.4624 25 38.5 25V25C41.5376 25 44 27.4624 44 30.5V70.5C44 73.5376 41.5376 76 38.5 76V76C35.4624 76 33 73.5376 33 70.5V30.5Z" fill="#36454F"/><path class="barOnSVG" d="M56 30.5C56 27.4624 58.4624 25 61.5 25V25C64.5376 25 67 27.4624 67 30.5V70.5C67 73.5376 64.5376 76 61.5 76V76C58.4624 76 56 73.5376 56 70.5V30.5Z" fill="#36454F"/></svg>');
 
-  stopwatchCycle = setInterval(stopwatch, 1000);
+  stopwatchCycle = setInterval(stopwatch, 1);
 }
 
 // Main code for stopwatch
 function stopwatch() {
   // increments stopwatch
-  stopwatchS += sValue;
+  stopwatchMs += sValue;
 
   // sorts out number changing
-  if (stopwatchS > 59) {
-    stopwatchS = 0;
-    stopwatchM += 1;
-    if (stopwatchM > 59) {
-      stopwatchM = 0;
-      stopwatchH += 1;
+  if (stopwatchMs > 99) {
+    stopwatchMs = 0;
+    stopwatchS += 1;
+    if (stopwatchS > 59) {
+      stopwatchS = 0;
+      stopwatchM += 1;
+      if (stopwatchM > 59) {
+        stopwatchM = 0;
+        stopwatchH += 1;
+      }
     }
   }
 
   // Print variables
+  pSMS = stopwatchMs;
   pSS = stopwatchS;
   pSM = stopwatchM;
   pSH = stopwatchH;
 
   // Pads integers
   if (doubleDigits === "true") {
+    pSMS = (pSMS < 10 ? "0" : "" ) + pSMS;
     pSS = (pSS < 10 ? "0" : "" ) + pSS;
     pSM = (pSM < 10 ? "0" : "" ) + pSM;
     pSH = (pSH < 10 ? "0" : "" ) + pSH;
@@ -946,6 +953,7 @@ function stopwatch() {
   $("#span4").html(pSH);
   $("#span5").html(pSM);
   $("#span6").html(pSS);
+  $("#span7").html(pSMS);
 }
 
 // Pauses stopwatch
@@ -965,9 +973,17 @@ function resetStopwatch() {
   clearInterval(stopwatchCycle);
 
   // Clears values
-  $("#span4").html("0");
-  $("#span5").html("0");
-  $("#span6").html("0");
+  if (doubleDigits == "true") {
+    $("#span4").html("00");
+    $("#span5").html("00");
+    $("#span6").html("00");
+    $("#span7").html("00");
+  } else {
+    $("#span4").html("0");
+    $("#span5").html("0");
+    $("#span6").html("0");
+    $("#span7").html("0");
+  }
 
   sValue = 1;
   $("#pauseStopwatch").html('<circle cx="50" cy="50" r="50" fill="#C4C4C4"/><path d="M33 30.5C33 27.4624 35.4624 25 38.5 25V25C41.5376 25 44 27.4624 44 30.5V70.5C44 73.5376 41.5376 76 38.5 76V76C35.4624 76 33 73.5376 33 70.5V30.5Z" fill="#36454F"/><path d="M56 30.5C56 27.4624 58.4624 25 61.5 25V25C64.5376 25 67 27.4624 67 30.5V70.5C67 73.5376 64.5376 76 61.5 76V76C58.4624 76 56 73.5376 56 70.5V30.5Z" fill="#36454F"/>');
